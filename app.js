678d751b4a2e1c3afe01a5f1933638608f0712d7
unknown <wahyusuteja688@gmail.com>
2023-12-10 17:36:53 +0700

    membuat contact app

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
 // Core Module
 // File System
-const fs = require('fs');
+// const fs = require('fs');
 
 // menuliskan string file (syncronous)
-try {
-    fs.writeFileSync('data/test.txt', "Hello World secara syncronous!!");
-} catch (e) {
-    console.log(e)
-}
+// try {
+//     fs.writeFileSync('data/test.txt', "Hello World secara syncronous!!");
+// } catch (e) {
+//     console.log(e)
+// }
 
 // menuliskan string file (Asyncronus)
-fs.writeFile("data/test.txt", "Hello World secara Asyncronous!!", (e) => {
-    console.log(e)
-})
+// fs.writeFile("data/test.txt", "Hello World secara Asyncronous!!", (e) => {
+//     console.log(e)
+// })
 
 // membaca file (Syncronous)
-const data = fs.readFileSync("data/contacts.json", 'utf-8');
-console.log(data);
+// const data = fs.readFileSync("data/contacts.json", 'utf-8');
+// console.log(data);
 
 // membaca file (Asyncronous)
-fs.readFile("data/test.txt", "utf-8", (err, data) => {
-    if (err) throw err;
-    console.log(data)
-})
+// fs.readFile("data/test.txt", "utf-8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data)
+// })
 
-// Readline
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const contacts = require("./contacts");
 
-rl.question("Masukan nama anda : ", (nama) => {
-    rl.question("Masukan no HP anda : ", (noHp) => {
-        const contact = { nama, noHp };
-        const file = fs.readFileSync("data/contacts.json", "utf8");
-        const contacts = JSON.parse(file);
+const main = async () => {
+    const nama = await contacts.tulisPertanyaan("Masukan nama anda :");
+    const email = await contacts.tulisPertanyaan("Masukan email anda :");
+    const noHp = await contacts.tulisPertanyaan("Masukan no Hp anda :");
 
-        contacts.push(contact);
+    contacts.simpanContact(nama, email, noHp);
+};
 
-        fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-
-        console.log("Terimakasih sudah menginput data")
-        
-            rl.close()
-});
-});
-
+main();
 
 
